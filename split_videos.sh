9fd5c419052732766440f9ff34f3e0dc89d61955
SachaWheeler <sacha@sachawheeler.com>
2020-02-06 10:29:38 +0000

    only process unprocessed folders

--- split_videos.sh
+++ split_videos.sh
@@ -3,7 +3,10 @@ for episode in video/*/*.mkv; do
         echo "splitting: $episode"
         directory=${episode%.*}
 
-        if [ -d "$directory" ]; then
-                scenedetect --input "$episode" --output "$directory" --stats scene_stats.csv detect-content split-video --high-quality
+        if [ -d "$directory" ] && [ ! -d "$directory/splits" ]; then
+                if [ ! -d "$directory/splits" ]; then
+                        mkdir "$directory/splits"
+                fi
+                scenedetect --input "$episode" --output "$directory/splits" --stats scene_stats.csv detect-content split-video --high-quality
         fi
 done
