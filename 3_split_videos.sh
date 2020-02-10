4c9b3f8d6fc1ecddbdcb98c96a44e1186cc59280
SachaWheeler <sacha@sachawheeler.com>
2020-02-10 14:50:33 +0000

    nice

--- 3_split_videos.sh
+++ 3_split_videos.sh
@@ -7,7 +7,7 @@ for episode in video/*/*.mkv; do
                 if [ ! -d "$directory/splits" ]; then
                         mkdir "$directory/splits"
                 fi
-                scenedetect --input "$episode" --output "$directory/splits" --stats scene_stats.csv detect-content split-video --high-quality
+                nice -10 scenedetect --input "$episode" --output "$directory/splits" --stats scene_stats.csv detect-content split-video --high-quality
                 touch "$directory/split_completed.txt"
         fi
 done
